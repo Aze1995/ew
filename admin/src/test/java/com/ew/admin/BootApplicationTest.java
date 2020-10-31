079f0c359504ac05104fd38df93a8791b710972f
ZeQun <hzq@wt.com>
2020-10-31 13:12:57 +0800

    添加测试方法

---
+++ admin/src/test/java/com/ew/admin/BootApplicationTest.java
@@ -0,0 +1,16 @@
+package com.ew.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+	
+}
